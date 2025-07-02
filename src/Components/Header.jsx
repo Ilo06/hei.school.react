75c4be04cf4e36c2180cbd58674835c32bd3bd6c
Ilo <hei.iloniaina@gmail.com>
2025-07-02 15:29:08 +0300

    feat: elements on header (navbar) have been placed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,17 @@ import logo from "../assets/hei.logo.png"
 function Header() {
     return(
         <>
-            <header>
-                <img src={logo} />
+            <header className="flex row justify-between items-center px-7 py-5 bg-blue-950">
+                <img src={logo} width={75} height={75}/>
+                <div className="links text-white">
+                    <ul className="flex row gap-10 items-center ">
+                        <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">ACCUEIL</a></li>
+                        <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">ACTUALITÉS</a></li>
+                        <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">BOURSE D'ÉTUDES</a></li>
+                        <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">INSCRIPTION</a></li>
+                        <li className="font-bold bg-amber-400 p-3 rounded-sm"><a href="http://">INTRANET</a></li>
+                    </ul>
+                </div>
             </header>
         </>
     );
