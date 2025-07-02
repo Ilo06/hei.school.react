f2cebd3d87cc7e2230ef17222b19413306fd14bf
Ilo <hei.iloniaina@gmail.com>
2025-07-02 11:56:42 +0300

    add: font family

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/hei.logo.png"
+
+function Header() {
+    return(
+        <>
+            <header>
+                <img src={logo} />
+            </header>
+        </>
+    );
+}
+
+export default Header
