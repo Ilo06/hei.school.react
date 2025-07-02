948da3ee89f7288481dc463d2e0d3284c7336fe1
Ilo <hei.iloniaina@gmail.com>
2025-07-02 15:36:09 +0300

    fix: add more inline padding on header and add some shadow on hei admin link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/hei.logo.png"
 function Header() {
     return(
         <>
-            <header className="flex row justify-between items-center px-7 py-5 bg-blue-950">
+            <header className="flex row justify-between items-center px-12 py-5 bg-blue-950">
                 <img src={logo} width={75} height={75}/>
                 <div className="links text-white">
                     <ul className="flex row gap-10 items-center ">
@@ -11,7 +11,7 @@ function Header() {
                         <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">ACTUALITÉS</a></li>
                         <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">BOURSE D'ÉTUDES</a></li>
                         <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">INSCRIPTION</a></li>
-                        <li className="font-bold bg-amber-400 p-3 rounded-sm"><a href="http://">INTRANET</a></li>
+                        <li className="font-bold bg-amber-400 p-3 rounded-sm shadow-md shadow-gray-800"><a href="http://">INTRANET</a></li>
                     </ul>
                 </div>
             </header>
