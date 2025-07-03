6e4f0eaa29065b94b573d8451d8af3216b29f8e2
Ilo <hei.iloniaina@gmail.com>
2025-07-03 11:54:19 +0300

    add: domains section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,15 +8,15 @@ function About(){
                 </div>
                 <div className='flex flex-col gap-7 about__description flex-1/2 text-left py-7'>
                     <h1 className='eb-garamond text-5xl'>À propos de nous</h1>
-                    <h2 className='flex row text-4xl font-semibold'><i className='bxr bx-education text-6xl text-secondary-color'></i> <h2 className='my-auto'>+3ans</h2></h2>
+                    <h2 className='flex row text-2xl font-bold'><i className='bxr bx-education text-6xl text-secondary-color'></i> <h2 className='my-auto'>+3ans</h2></h2>
                     <p className='text-lg'>Depuis 3 ans, nous formons ceux qui décodent le futur, aux métiers de l’informatique.
                         Nous sommes un groupe d’experts, de professionnels et des passionnés de l’informatique de
                         Madagascar et du monde entier.</p>
-                    <h2 className='flex row text-4xl font-semibold'><i className='bxr  bx-people-diversity text-6xl text-secondary-color'></i> <h2 className='my-auto'>+250 étudiants</h2></h2>
+                    <h2 className='flex row text-2xl font-bold'><i className='bxr  bx-people-diversity text-6xl text-secondary-color'></i> <h2 className='my-auto'>+250 étudiants</h2></h2>
                     <p className='text-lg'>Plus de 250 étudiants et plus d’une dizaine d’entreprises ont déjà fait confiance à notre
                         formation habilitée par l’État malgache. Avec eux tous, nous sommes fiers de pouvoir parler de
                         la “grande famille HEI”.</p>
-                    <h2 className='flex row text-4xl font-semibold'><i className='bxr  bx-medal-star text-6xl text-secondary-color'></i> <h2 className='my-auto'>Notre mission</h2></h2>
+                    <h2 className='flex row text-2xl font-bold'><i className='bxr  bx-medal-star text-6xl text-secondary-color'></i> <h2 className='my-auto'>Notre mission</h2></h2>
                     <p className='text-lg'>Notre mission est d’amener des jeunes malgaches sur des domaines porteurs comme l’intelligence
                         artificielle, la cybersécurité ou la programmation afin de favoriser leur employabilité et le
                         développement de Madagascar.</p>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,11 @@
-function Card(params) {
-    
+function Card({ image, title, text }) {
+  return (
+    <div className="bg-white p-6 rounded-2xl max-w-sm hover:shadow-lg transition duration-300 w-1/3 py-6">
+      <img src={image} alt={title} className=" mx-auto mb-4 w-36 h-36" />
+      <h2 className="text-xl font-bold text-secondary-color text-center eb-garamond text-[24px]">{title}</h2>
+      <p className="text-center text-primary-color mt-4 text-[17px]">{text}</p>
+    </div>
+  );
 }
 
 export default Card;
