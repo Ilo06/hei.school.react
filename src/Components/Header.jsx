a2a158a41eb2255ce1f6d5dfc9122b64d78983d1
Ilo <hei.iloniaina@gmail.com>
2025-07-03 09:34:43 +0300

    fix: fixed color

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,15 +3,15 @@ import logo from "../assets/hei.logo.png"
 function Header() {
     return(
         <>
-            <header className="flex row justify-between items-center px-12 py-5 bg-blue-950">
+            <header className="flex row justify-between items-center px-12 py-3  bg-primary-color">
                 <img src={logo} width={75} height={75}/>
                 <div className="links text-white">
                     <ul className="flex row gap-10 items-center ">
-                        <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">ACCUEIL</a></li>
-                        <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">ACTUALITÉS</a></li>
-                        <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">BOURSE D'ÉTUDES</a></li>
-                        <li className="font-bold hover:text-amber-400 transition-all"><a href="http://">INSCRIPTION</a></li>
-                        <li className="font-bold bg-amber-400 p-3 rounded-sm shadow-md shadow-gray-800"><a href="http://">INTRANET</a></li>
+                        <li className="font-bold hover:text-secondary-color transition-all"><a href="http://">ACCUEIL</a></li>
+                        <li className="font-bold hover:text-secondary-color transition-all"><a href="http://">ACTUALITÉS</a></li>
+                        <li className="font-bold hover:text-secondary-color transition-all"><a href="http://">BOURSE D'ÉTUDES</a></li>
+                        <li className="font-bold hover:text-secondary-color transition-all"><a href="http://">INSCRIPTION</a></li>
+                        <li className="font-bold bg-secondary-color p-3 rounded-sm shadow-md shadow-gray-800"><a href="https://admin.hei.school">INTRANET</a></li>
                     </ul>
                 </div>
             </header>
