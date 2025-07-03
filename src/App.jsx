13698c65ef7d0145a032d4106f19be706183d4ea
Vaniaina <hei.vaniaina@gmail.com>
2025-07-03 10:47:01 +0300

    [FIX] - About font

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import Header from './Components/Header.jsx'
 import Outlets from './Components/Outlets.jsx'
+import About from "./Components/About.jsx";
 
 function App() {
 
   return (
     <>
         <Header/>
+        <About/>
         <Outlets/>
     </>
   )
