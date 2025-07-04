47d0baa885bed80b927b9a059b1da85c4785c801
Vaniaina <hei.vaniaina@gmail.com>
2025-07-04 12:28:30 +0300

    [ADD] - Admission + partner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-
 import './App.css'
 import About from './Components/About.jsx'
 import Domains from './Components/Domains.jsx'
 import Header from './Components/Header.jsx'
 import Outlets from './Components/Outlets.jsx'
 import Technology from "./Components/Technology.jsx";
+import Partner from "./Components/Partner.jsx";
+import Admission from "./Components/Admission.jsx";
 
 
 function App() {
 
-  return (
-    <>
-        <Header/>
-        <About/>
-        <Outlets/>
-       <Domains/>
-        <Technology/>
-      </>
-  )
+    return (
+        <>
+            <Header/>
+            <About/>
+            <Outlets/>
+            <Domains/>
+            <Admission/>
+            <Technology/>
+            <Partner/>
+        </>
+    )
 }
 
 export default App
